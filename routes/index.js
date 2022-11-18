37abe0c3af50413e3d5b2f9e1d2787c403fcba34
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2022-11-17 21:47:27 -0400

    Buen tercio con github
    solamente cambie el titulo del index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ManPower' });
+  res.render('index', { title: 'Buen tercio con github' });
 });
 
 module.exports = router;
