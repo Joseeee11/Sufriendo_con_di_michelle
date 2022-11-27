8999e8d164a7d60f434f727706b3d3be1a47929a
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2022-11-26 22:03:18 -0400

    ARREGLADO (PERO CON CADENA DE TEXTO)
    EL ID ESTA COMO STRING DE MOMENTO, para que sirva el filter, ya que el valor que llega de la URL es siempre un STRING

--- Prueba_avion.js
+++ Prueba_avion.js
@@ -1,23 +1,23 @@
 let Aviones = [{
     Marca:'chini',
-    id:1,
+    id:'1',
     Pais:'china',
     Modelo:'8DGC'
   },{
     Marca:'tercio',
-    id:2,
+    id:'2',
     Pais:'Brasil',
     Modelo:'F8G7'
   },{
     Marca:'Cuarteto',
-    id:3,
+    id:'3',
     Pais:'Argentina',
     Modelo:'G89G'
   },{
     Marca:'Roko',
-    id:4,
+    id:'4',
     Pais:'Groelandia',
     Modelo:'7P3G'
   }]
   
-  module.exports.Aviones = Aviones;  
+  module.exports = Aviones;  
