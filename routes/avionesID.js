3e251673619b3129e30feb26ec5cc9c9504c4106
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2022-11-26 22:34:19 -0400

    Arregle el problema con el String

--- routes/avionesID.js
+++ routes/avionesID.js
@@ -4,13 +4,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {  
-    const id = req.params.id  // http://localhost:3000/avionesID/*ID DE LO QUE SE BUSCA*
+    const id = Number(req.params.id)  // http://localhost:3000/avionesID/*ID DE LO QUE SE BUSCA*
     const resultado = Aviones.filter(Encontrado => Encontrado.id === id)
     if (resultado.length === 0) {
         res.send(`No se logro encontrar ${id}`)
     }else{
     res.send(resultado)
     }
+    
 
 //     res.send((req,res)=>{
         
