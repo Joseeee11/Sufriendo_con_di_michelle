c0724454b9936e778e945441ce263eadf83071b3
rossjbm <rosimarbarrios0301@gmail.com>
2022-12-04 19:40:06 -0400

    detalles xd

--- controladores/e.crear.js
+++ controladores/e.crear.js
@@ -3,7 +3,7 @@ const {equipo} = require('../datosBD/info.js').infoMantenimiento;
 class crearEquipo {
     crear(req, res, next) {
         var nuevoEquipo = req.body;
-        var existente = equipo.filter(t => t.id == nuevoEquipo.id);
+        var existente = equipo.filter(e => e.id == nuevoEquipo.id);
         if (existente.length === 0) {
             equipo.push(nuevoEquipo);
         } else {
