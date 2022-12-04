7af77735069f7ce06512721566ef8ccd49ecaade
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2022-12-04 01:25:30 -0400

    No se

--- routes/equipo.js
+++ routes/equipo.js
@@ -2,18 +2,19 @@ var Equipo_BD = require('../Equipo_BD.json')
 var express = require('express');
 var router = express.Router();
 const Buscar_equipo = require('../controladores/Buscar_equipo')
-
+var Equipo_E = Equipo_BD.Equipo
+var Equipo_M = Equipo_BD.Mantenimiento
 /* Controladores */
 let resultado = new Buscar_equipo()
 
 /* RUTAS */
 router.get('/', function(req, res, next) {
-  resultado.Todos(req,res,next,Equipo_BD)
+  resultado.Todos(req,res,next,Equipo_E)
 });
 
 // http://localhost:3000/avionesID/*ID DE LO QUE SE BUSCA*
 router.get('/:id', function(req, res, next) {  
-    resultado.BuscarPorID(req,res,next,Equipo_BD)
+    resultado.BuscarPorID(req,res,next,Equipo_E)
 });
 
 
